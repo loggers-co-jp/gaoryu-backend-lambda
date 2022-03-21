0be3491396d58bd304dc2e82761b04c92cd1e1ed
quarkusio <no-reply@quarkus.io>
2022-03-21 08:42:03 +0000

    Initial commit

---
+++ src/test/java/jp/co/loggers/lambda/LambdaHandlerTestIT.java
@@ -0,0 +1,9 @@
+package jp.co.loggers.lambda;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class LambdaHandlerTestIT extends LambdaHandlerTest {
+
+    // Execute the same tests but in native mode.
+}
